0734e75de041c482958e0fa64cf0ad42b0529079
pratiksontakke <pratikass488@gmail.com>
2020-06-30 09:34:20 +0530

    updated file with mongooose and mongoseDB

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejsLint = require('ejs-lint');
 const mymodule = require(__dirname + "/mymodule.js");
+const mongoose = require('mongoose');
 const app = express();
 
 app.use(express.static('public'))
@@ -10,20 +11,103 @@ app.use(bodyParser.urlencoded({
   extended: true
 }))
 
+// Creating mongoose database and add default items
+
+mongoose.connect('mongodb://localhost:27017/todolistDB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false
+});
+
+const itemsSchema = {
+  task: {
+    type: String,
+    required: true
+  }
+};
+const Item = mongoose.model('Item', itemsSchema);
+
+
+const Item1 = new Item({
+  task: "Exercise"
+});
+const Item2 = new Item({
+  task: "Meditaion"
+});
+const Item3 = new Item({
+  task: "Surya Aasan"
+});
+
+const defaultItems = [Item1, Item2, Item3];
+
+const listSchema = {
+  name: String,
+  items: [itemsSchema]
+};
+
+const List = mongoose.model("List", listSchema)
+
+// Creating mongoose database and add default items CLOSED
+
 app.set("view engine", "ejs");
-//Decleare global variables
-const list_item = [];
-const workItem = [];
 
 app.get("/", function(req, res) {
 
   let day = mymodule.date();
-  res.render("list", {
-    listTitle: day,
-    items: list_item,
+  Item.find({}, function(err, foundItem) {
+    if (foundItem.length === 0) {
+      Item.insertMany(defaultItems, function(err, foundItem) {
+        if (err) {
+          console.log("Error = " + err);
+
+        } else {
+          console.log('Default items successfully insert, foundItem length = ' + foundItem.length);
+          res.redirect("/");
+
+        }
+      });
+    } else {
+      res.render("list", {
+        listTitle: day,
+        items: foundItem
+      })
+      console.log('foundItem length = ' + foundItem.length);
+    }
   })
 
-})
+
+}); //app.get("/") closed
+
+
+app.get("/:customListName", function(req, res) {
+  const customListName = req.params.customListName;
+  List.findOne({
+    name: customListName
+  }, function(err, foundList) {
+    if (!err) {
+      if (!foundList) { // not found the customListName in foundList then execute if code
+        //Creating a new list or add our default list
+        console.log("doesn't exists");
+        const list = new List({
+          name: customListName,
+          items: defaultItems
+        });
+        list.save();
+        // res.redirect("/" + customListName);
+      } else {
+        // show existing list
+        res.render("list", {
+          listTitle: foundList.name,
+          items: foundList.items
+        });
+        console.log("exists");
+      }
+      console.log("Error = " + err);
+
+    }
+
+  });
+}); //app.get("/:customListName") closed
 
 app.post("/", function(req, res) {
 
@@ -38,37 +122,33 @@ app.post("/", function(req, res) {
     console.log(req.body);
   }
 
-  let string = req.body.newItem
-  justItem = string.trim()
-  justItem = mymodule.capitalfirstlatter(justItem)
-  if (justItem === "") { //to ovide white space
-    var typeerror = "Type some task";
-    console.log(typeerror);
-    if (req.body.list === "Work List") {
-      res.redirect("/work");
+  let itemName = req.body.newItem;
+  itemName = itemName.trim()
+  itemName = mymodule.capitalfirstlatter(itemName)
+  item = new Item({
+    task: itemName
+  });
+  item.save();
+  res.redirect("/");
+}); //app.post("/") closed
+
+app.post("/delete", function(req, res) {
+  const Checkbox = req.body.newCheckbox;
+  console.log("id = " + Checkbox + " successfully deleted.");
+  Item.findByIdAndRemove(Checkbox, function(err) {
+    if (err) {
+      console.log("Error = " + err);
     } else {
       res.redirect("/");
     }
-    console.log(req.body);
-  } else {
-    specificpush();
-  }
-
-})
-
-app.get("/work", function(req, res) {
-  res.render("list", {
-    listTitle: "Work List",
-    items: workItem,
-  })
-
-})
+  });
+}); //app.post("/delete") CLOSED
 
 app.get("/about", function(req, res) {
   res.render("about");
-})
+});
 
 
 app.listen(3000, function() {
   console.log("server is running on port 3000 !")
-})
+});
